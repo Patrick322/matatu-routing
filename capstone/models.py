2fdc11d19bfd75c013ba5d16dd1b2c0ac2789520
Patrick595 <patrickbuong@gmail.com>
2020-04-02 04:13:21 +0300

    added class profile in the models

--- capstone/models.py
+++ capstone/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-from django import * 
-from django import Posts  
+import datetime as dt
+from django .contrib.auth.models import User
+from tinymce.models import HTMLField
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from cloudinary.models import CloudinaryField 
 
 # Create your models here.
 class UserProfile(models.Model):
